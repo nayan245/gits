d35d297be7964b217db5569e10cec6ad8a0ec589
UTKARSHA GHAGARE <utkarshaghagare03@gmail.com>
2023-07-19 00:40:54 +0530

    fabonacci series implemented.

--- diff.java
+++ diff.java
@@ -1,3 +1,16 @@
 public class diff {
-    test Git
+    test Git;
+    public static void main(String args[]){
+        int n1= 0;
+        int n2= 1;
+        int count =10;
+        System.out.println(n1);
+        System.out.println(n1);
+        for(int i=0; i<=count; i++){
+            int n3= n1+n2;
+            System.out.println(n3);
+            n1=n2;
+            n2=n3;
+        }
+    }
 }
